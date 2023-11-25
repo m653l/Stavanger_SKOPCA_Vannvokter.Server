c6d6b1d6a1831a035a976043e3d921fb0cda20cd
M6553365533l <sebastian.kura@pm.me>
2023-11-25 20:24:31 +0100

    q

--- Application/Producers/Dtos/ProducerDto.cs
+++ Application/Producers/Dtos/ProducerDto.cs
@@ -5,7 +5,7 @@ namespace Application.Producers.Dtos
 {
     public class ProducerDto : IMapFrom<Producer>, IMapTo<Producer>
     {
-        public bool isPies { get; set; } = false;
+        public bool? isPies { get; set; } = false;
 
         public string Name { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
